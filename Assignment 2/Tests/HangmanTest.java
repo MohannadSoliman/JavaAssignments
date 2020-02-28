a4d9c7f5afab6289e4e91f23fd2a650f256ef89e
MohannadSoliman <61092652+MohannadSoliman@users.noreply.github.com>
2020-02-28 20:35:40 +0200

    Add files via upload

---
+++ Assignment 2/Tests/HangmanTest.java
@@ -0,0 +1,48 @@
+package eg.edu.alexu.csd.datastructure.hangman.cs23;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HangmanTest {
+	Hangman obj = new Hangman();
+	String words[] = {"hello", "welcome", "trial"};
+	@Test
+	public void testSetDictionary() {
+		obj.setDictionary(words);
+		assertNotNull(obj.dictionary[0]);
+	}
+
+	@Test
+	public void testSelectRandomSecretWord() {
+		obj.setDictionary(words);
+		obj.selectRandomSecretWord();
+		assertNotNull(obj.chosenWord);
+	}
+
+
+	@Test
+	public void testSetMaxWrongGuesses() {
+		obj.setMaxWrongGuesses(5);
+		assertNotNull(obj.maximum);
+	}
+
+	@Test
+	public void testGuess() throws Exception{
+		obj.chosenWord = "hello";
+		obj.maximum = 3;
+		assertEquals("-----", obj.guess('a'));
+		assertEquals("h----", obj.guess('H'));
+		assertEquals("h-ll-", obj.guess('l'));
+		obj.maximum = 0;
+		assertEquals(null, obj.guess('e'));
+	}
+	@Test
+	(expected = Exception.class)
+	public void buggyWord() throws Exception
+	{
+		obj.chosenWord = "he&llo";
+		obj.guess('a');
+	}
+
+}
